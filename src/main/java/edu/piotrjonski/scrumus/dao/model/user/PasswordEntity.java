1dc9d0b90197d5f40b2c9f03e547e128520cfd0c
Piotr Joński <yoyo@wp.eu>
2015-12-08 20:10:28 +0100

    SCRUMUS-19. Password PasswordEntity, PasswordDAO PasswordDAOTest

--- src/main/java/edu/piotrjonski/scrumus/dao/model/user/PasswordEntity.java
+++ src/main/java/edu/piotrjonski/scrumus/dao/model/user/PasswordEntity.java
@@ -12,7 +12,11 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "password")
+@NamedQueries({@NamedQuery(name = PasswordEntity.FIND_ALL, query = PasswordEntity.FIND_ALL_QUERY)})
 public class PasswordEntity {
+
+    public static final String FIND_ALL = "findAllPasswords";
+    protected static final String FIND_ALL_QUERY = "SELECT d FROM PasswordEntity d";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
