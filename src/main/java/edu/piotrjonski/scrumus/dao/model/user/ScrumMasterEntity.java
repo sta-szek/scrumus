d31a2c80bcbb2fae8972568af1cc83213d4abff0
Piotr Joński <yoyo@wp.eu>
2015-12-08 19:06:25 +0100

    SCRUMUS-19. ProductOwner + DAO

--- src/main/java/edu/piotrjonski/scrumus/dao/model/user/ScrumMasterEntity.java
+++ src/main/java/edu/piotrjonski/scrumus/dao/model/user/ScrumMasterEntity.java
@@ -19,7 +19,7 @@ public class ScrumMasterEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, optional = false)
     private DeveloperEntity developerEntity;
 
     @OneToMany
