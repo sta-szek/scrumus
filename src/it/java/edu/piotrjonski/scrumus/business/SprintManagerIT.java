865f5860d6731138b7f55e672087ce56f63303fa
Piotr Joński <yoyo@wp.eu>
2015-12-22 19:06:08 +0100

    SCRUMUS-34. Testy do SprintManager

--- src/it/java/edu/piotrjonski/scrumus/business/SprintManagerIT.java
+++ src/it/java/edu/piotrjonski/scrumus/business/SprintManagerIT.java
@@ -21,6 +21,7 @@ import javax.transaction.NotSupportedException;
 import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -74,6 +75,36 @@ public class SprintManagerIT {
 
         // then
         assertThat(result).isTrue();
+    }
+
+    @Test
+    public void shouldFindSprint() throws AlreadyExistException {
+        // given
+        Sprint sprint = createSprint();
+        Sprint savedSprint = sprintManager.createSprint(sprint);
+
+        // when
+        Sprint result = sprintManager.findSprint(savedSprint.getId());
+
+        // then
+        assertThat(result).isEqualTo(savedSprint);
+    }
+
+    @Test
+    public void shouldFindAllSprintsForProject() throws AlreadyExistException {
+        // given
+        Sprint sprint1 = createSprint();
+        Sprint sprint2 = createSprint();
+        sprint1 = sprintManager.createSprint(sprint1);
+        sprint2 = sprintManager.createSprint(sprint2);
+
+        // when
+        List<Sprint> result = sprintManager.findAllSprintsForProject(lastProject.getKey());
+
+        // then
+        assertThat(result).hasSize(2)
+                          .contains(sprint1)
+                          .contains(sprint2);
     }
 
     private void startTransaction() throws SystemException, NotSupportedException {
