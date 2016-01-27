4b59447407578e49af05904104540fc1c996939a
Piotr Joński <yoyo@wp.eu>
2016-01-27 19:04:11 +0100

    SCRUMUS-42. Wyświetlanie backlogu na widoku projektu

--- src/main/java/edu/piotrjonski/scrumus/ui/services/AgileService.java
+++ src/main/java/edu/piotrjonski/scrumus/ui/services/AgileService.java
@@ -1,16 +1,40 @@
 package edu.piotrjonski.scrumus.ui.services;
 
+import edu.piotrjonski.scrumus.business.IssueManager;
+import edu.piotrjonski.scrumus.business.ProjectManager;
+import edu.piotrjonski.scrumus.business.SprintManager;
+import edu.piotrjonski.scrumus.domain.Backlog;
+import edu.piotrjonski.scrumus.domain.Sprint;
 import lombok.Data;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 public class AgileService implements Serializable {
 
     @Inject
     private transient Logger logger;
+
+    @Inject
+    private ProjectManager projectManager;
+
+    @Inject
+    private IssueManager issueManager;
+
+    @Inject
+    private SprintManager sprintManager;
+
+    public List<Sprint> findSprintsForProject(String projectKey) {
+        return sprintManager.findAllSprintsForProject(projectKey);
+    }
+
+    public Backlog findBacklogForProject(String projectKey) {
+        return projectManager.findBacklogForProject(projectKey)
+                             .orElse(null);
+    }
 
 
 }
