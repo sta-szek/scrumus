25e349e4d152b0913179be5fa20a8d60246dacee
Piotr Joński <yoyo@wp.eu>
2016-01-28 14:07:51 +0100

    SCRUMUS-42. Wyswietlanie listy stories w liscie sprintow

--- src/main/java/edu/piotrjonski/scrumus/ui/services/AgileService.java
+++ src/main/java/edu/piotrjonski/scrumus/ui/services/AgileService.java
@@ -3,13 +3,16 @@ package edu.piotrjonski.scrumus.ui.services;
 import edu.piotrjonski.scrumus.business.IssueManager;
 import edu.piotrjonski.scrumus.business.ProjectManager;
 import edu.piotrjonski.scrumus.business.SprintManager;
+import edu.piotrjonski.scrumus.business.StoryManager;
 import edu.piotrjonski.scrumus.domain.Backlog;
 import edu.piotrjonski.scrumus.domain.Sprint;
+import edu.piotrjonski.scrumus.domain.Story;
 import lombok.Data;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -27,6 +30,9 @@ public class AgileService implements Serializable {
     @Inject
     private SprintManager sprintManager;
 
+    @Inject
+    private StoryManager storyManager;
+
     public List<Sprint> findSprintsForProject(String projectKey) {
         return sprintManager.findAllSprintsForProject(projectKey);
     }
@@ -34,6 +40,15 @@ public class AgileService implements Serializable {
     public Backlog findBacklogForProject(String projectKey) {
         return projectManager.findBacklogForProject(projectKey)
                              .orElse(null);
+    }
+
+    public List<Story> findStoriesForSprint(String sprintId) {
+        try {
+            int sprintIntId = Integer.parseInt(sprintId);
+            return storyManager.findAllStoriesForSprint(sprintIntId);
+        } catch (NumberFormatException e) {
+            return new ArrayList<>();
+        }
     }
 
 
